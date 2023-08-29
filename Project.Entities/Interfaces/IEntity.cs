8feaf485b63ba8a175a3753720c33e9637c1b769
Hasan Emir Güven <emirgv15@gmail.com>
2023-08-29 16:29:58 +0300

    BaseEntity and IEntity Created

---
+++ Project.Entities/Interfaces/IEntity.cs
@@ -0,0 +1,27 @@
+﻿using Project.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Entities.Interfaces
+{
+    public interface IEntity
+    {
+        int ID { get; set; }
+
+        // Dates
+        DateTime CreatedDate { get; set; }
+        DateTime UpdatedDate { get; set; }
+        DateTime DeletedDate { get; set; }
+
+        // Users
+        string? CreatedBy { get; set; }
+        string? UpdatedBy { get; set; }
+        string? DeletedBy { get; set; }
+
+        // Status (ENUM)
+        DataStatus Status { get; set; }
+    }
+}
